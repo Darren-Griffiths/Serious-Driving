f4a88bd0d2bfea7172da74e777ee70edd097e2e1
Darren <darrengriffiths316@outlook.com>
2018-09-24 15:34:45 +0100

    Video Test

---
+++ Unity Project Versions/Serious Driving/Assets/Scripts/Level 1/video.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class video : MonoBehaviour {
+
+    public GameObject video1;
+    public VideoClip video2;
+    public VideoPlayer video3;
+
+	// Use this for initialization
+	void Start () {
+
+        video3.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
