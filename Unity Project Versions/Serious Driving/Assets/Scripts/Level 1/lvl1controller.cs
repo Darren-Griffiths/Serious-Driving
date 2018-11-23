5de3575768c7918f668c53bcbe32e870215ebbbd
Darren <darrengriffiths316@outlook.com>
2018-11-23 15:46:49 +0000

    pause menu functional with 4 options and link to 2 missions

--- Unity Project Versions/Serious Driving/Assets/Scripts/Level 1/lvl1controller.cs
+++ Unity Project Versions/Serious Driving/Assets/Scripts/Level 1/lvl1controller.cs
@@ -1,16 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class lvl1controller : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public GameObject pauseMenu;
+    public GameObject scenarios;
+    //public GameObject pauseMenu;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        //Checks for ESC press
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //If ESC is pressed, pause function is called.
+            pauseGame();
+        }
 	}
+
+
+    //Pause Game Function
+    public void pauseGame()
+    {
+        //Checks if the game is paused, if so resumes.
+        if (Time.timeScale == 0)
+        {
+            //Resume game
+            Time.timeScale = 1;
+            //Hides pause menu
+            pauseMenu.SetActive(false);
+            //Set Cursor to not be visible
+            Cursor.visible = false;
+        }
+
+        //Checks if the game is playing, if so pauses.
+        else if (Time.timeScale == 1)
+        {
+            //Freezes game
+            Time.timeScale = 0;
+            //Shows pause menu
+            pauseMenu.SetActive(true);
+            //Set Cursor to be visible
+            Cursor.visible = true;
+        }
+    }
+
+    //Opens scenarios
+    public void openScenarios()
+    {
+        scenarios.SetActive(true);
+    }
+
+    //Returns to main menu
+    public void mainmenuOnClick()
+    {
+        SceneManager.LoadScene("mainmenu");
+    }
+
+    //Exits game
+    public void exitgameOnClick()
+    {
+        Application.Quit();
+    }
 }
