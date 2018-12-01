801970b37c6439da0ed8a9600b408f71414231b4
Darren <darrengriffiths316@outlook.com>
2018-12-01 19:21:29 +0000

    unity anitmation attempt

---
+++ Unity Project Versions/Serious Driving/Assets/waypoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypoint : MonoBehaviour {
+
+    public bool playAutomatically;
+
+    // Use this for initialization
+    void Start () {
+
+        playAutomatically = true;
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
